eb95466779e99b954d0e98cdd67b3239fdade207
Bryan Cornejo <20230223@ricaldone.edu.sv>
2024-05-01 21:09:21 -0600

    Emulador finalizado

---
+++ Emulador/app/src/main/java/bryan/cornejo/emulador20230223bryan/Login.kt
@@ -0,0 +1,27 @@
+package bryan.cornejo.emulador20230223bryan
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+       val btnAcceder = findViewById<Button>(R.id.btnAcceder)
+        val siguiente = Intent(this, MainActivity::class.java)
+       btnAcceder.setOnClickListener{startActivity(siguiente)}
+
+
+    }
+}
